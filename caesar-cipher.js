850b9b3e449a0c3a7adac12e601f84ca2b7e6265
Souher Al-Taleb <altalebsouher8@gmail.com>
2025-11-04 22:44:44 +0100

    test

---
+++ caesar-cipher.js
@@ -0,0 +1,18 @@
+/* TASK: create a simple rock-paper-scissor game using node in terminal */
+/* ==================================================================== */
+/* author: Nagel, Dandy - date: 11/2025 ------------------------------- */
+
+/* 
+
+ROCK-PAPER_SCISSOR-RULES:
+- rock beats scissor (blunts them)
+- scissor beats paper (cuts it)
+- paper beats rock (covers it)
+
+REQUIREMENTS:
+[x] program should take the players move as input from process.argv
+[x] program should randomly generate a move for the computer
+[x] Determine the result based on the rules of Rock Paper Scissors
+[x] Output the result (win, lose, or draw) to the console
+
+*/
